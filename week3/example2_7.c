dc67f5536bfa949a6da45b3c4d52da98b57816a4
kyoohyunlee <khyhlee@naver.com>
2024-10-07 11:57:57 +0900

    example2_7.c 수정

--- week3/example2_7.c
+++ week3/example2_7.c
@@ -2,13 +2,15 @@
 
 int main() {
     int num; char ch;
-    unsigned int num2;
+
 
     num = 128;
-    num2 = num;
+
     ch = num;
     printf("%d %d\n", num, ch);
-    printf("%d %d %d\n", num, ch, num2);
+    printf("%d %d\n", num, ch);
+
+    //ch가 왜 -128이야
 
     // unsigned 절댓값 정수 표현 두배만큼 더 할 수 있음. 왜? 음수 표현 안하니깐
 
