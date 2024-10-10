5e8f52a1d59242ba9e2029567b78fa37c0b8431a
kyoohyunlee <khyhlee@naver.com>
2024-10-10 18:23:17 +0900

    Add ex5-9 true or false

---
+++ week5/example5_9.c
@@ -0,0 +1,14 @@
+//
+// Created by 이규현 on 2024. 10. 10..
+//
+#include <stdio.h>
+int main() {
+    int a = 1, b = 0;
+
+    if(a)
+      if(b)
+        printf("a and b are both true.\n");
+    else
+      printf("a is not true.\n");
+    return 0;
+}
