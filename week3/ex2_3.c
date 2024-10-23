43c91e54eb82e7cfc7dc8a4c101e36a47bbb8866
kyoohyunlee <khyhlee@naver.com>
2024-10-23 22:52:01 +0900

    dev. ex2_3

---
+++ week3/ex2_3.c
@@ -0,0 +1,13 @@
+//
+// Created by 이규현 on 2024. 10. 23..
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+
+int main(){
+  printf("sizeof(long)=%d.\n",sizeof(long));
+
+  printf("range of double is %e to %e.\n", DBL_MIN, DBL_MAX);
+  return 0;
+}
