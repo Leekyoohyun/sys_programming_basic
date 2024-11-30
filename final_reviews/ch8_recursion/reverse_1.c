b522cc2c3697085c3585f0a4e927ba531f0888c4
kyoohyunlee <khyhlee@naver.com>
2024-11-30 17:32:12 +0900

    dev: reverse2

--- final_reviews/ch8_recursion/reverse_1.c
+++ final_reviews/ch8_recursion/reverse_1.c
@@ -4,6 +4,7 @@
 //return; 하나로 아무 일도 하지말고 빠져나와라씀
 #include <stdio.h>
 
+/*
 void reverse(int n){
   if(n==0)
     return;
@@ -19,5 +20,21 @@ int main(){
   reverse(num);
   printf("\n");
 
+  return 0;
+}
+*/
+
+void reverse(int n){
+  if(n!=0){
+    printf("%d ",n%10);
+    reverse(n/10);
+  }
+}
+
+int main(){
+  int num;
+  scanf("%d",&num);
+  reverse(num);
+  printf("\n");
   return 0;
 }
