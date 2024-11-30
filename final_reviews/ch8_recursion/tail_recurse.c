82d89d43c40d5b9b538d7c06691d4085f526547a
kyoohyunlee <khyhlee@naver.com>
2024-11-30 17:12:25 +0900

    dev: tail recursion

--- final_reviews/ch8_recursion/tail_recurse.c
+++ final_reviews/ch8_recursion/tail_recurse.c
@@ -1,3 +1,21 @@
 //
 // Created by 이규현 on 2024. 11. 30..
 //
+#include <stdio.h>
+
+void recurse(int n){
+  if(n == 0)
+    return;
+  else{
+    printf("%d ", n);
+    recurse(n-1);
+  }
+}
+
+int main(){
+  int num;
+  printf("Enter a number: \n");
+  scanf("%d", &num);
+  recurse(num);
+
+}
